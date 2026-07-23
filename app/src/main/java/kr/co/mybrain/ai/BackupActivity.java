71448e006fd145ac71b1289b6dd7536bff820a47
KANG77556 <204034@gmail.com>
2026-07-23 09:35:43 +0900

    refactor: 백업 화면의 사용하지 않는 코드 제거

--- app/src/main/java/kr/co/mybrain/ai/BackupActivity.java
+++ app/src/main/java/kr/co/mybrain/ai/BackupActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.LinearLayout;
