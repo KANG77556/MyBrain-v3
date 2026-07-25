077181da9074bcd39eca5a3f54eb6eba1b110d8f
KANG77556 <204034@gmail.com>
2026-07-25 12:41:31 +0900

    ActivityNotFoundException import 경로 수정

--- app/src/main/java/kr/co/mybrain/ai/QuickInputActivity.java
+++ app/src/main/java/kr/co/mybrain/ai/QuickInputActivity.java
@@ -2,8 +2,8 @@ package kr.co.mybrain.ai;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.ActivityNotFoundException;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
