e32e08460f263d8870f47cd86f1d23bf4238707f
KANG77556 <204034@gmail.com>
2026-08-01 08:18:51 +0900

    feat: raise room schema to version two

---
+++ android-build/feature-overlay/app/src/main/java/com/seongho/brainassistant/core/database/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.seongho.brainassistant.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [
+        InputEntity::class,
+        NoteEntity::class,
+        TaskEntity::class,
+        CalendarEntity::class,
+        AnalysisEntity::class,
+        SyncOutboxEntity::class,
+        DDayEntity::class,
+        WidgetConfigEntity::class,
+        WidgetSnapshotEntity::class,
+    ],
+    version = 2,
+    exportSchema = true,
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun inputDao(): InputDao
+    abstract fun noteDao(): NoteDao
+    abstract fun taskDao(): TaskDao
+    abstract fun calendarDao(): CalendarDao
+    abstract fun analysisDao(): AnalysisDao
+    abstract fun syncOutboxDao(): SyncOutboxDao
+}
